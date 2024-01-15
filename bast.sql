57de813488595c6450f14413e4a9201a4d6ad7d0
M N Mahmud <bussines.mnmahmud@gmail.com>
2024-01-15 22:06:44 +0700

    update backup sql

--- bast.sql
+++ bast.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2023 at 11:07 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Jan 15, 2024 at 04:05 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,6 +34,7 @@ CREATE TABLE `bast_report` (
   `id_user_submitted` int(3) NOT NULL,
   `id_user_accepted` int(3) NOT NULL,
   `notes` text NOT NULL,
+  `user_id` int(2) DEFAULT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `created_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -49,7 +50,9 @@ CREATE TABLE `bast_report_details` (
   `id_bast` varchar(50) NOT NULL,
   `id_good` int(11) NOT NULL,
   `img_evidence` varchar(50) DEFAULT NULL,
-  `notes` text NOT NULL,
+  `notes` text DEFAULT NULL,
+  `type` int(1) NOT NULL,
+  `user_id` int(2) DEFAULT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `created_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -65,8 +68,20 @@ CREATE TABLE `branch` (
   `initial` varchar(10) NOT NULL,
   `name` varchar(50) NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `created_at` datetime NOT NULL
+  `created_at` datetime NOT NULL,
+  `created_by` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `branch`
+--
+
+INSERT INTO `branch` (`id`, `initial`, `name`, `updated_at`, `created_at`, `created_by`) VALUES
+(1, 'HOJKT', 'HEAD OFFICE JAKARTA', '2024-01-12 16:06:42', '2024-01-12 17:05:33', 1),
+(2, 'WSGNP', 'WORKSHOP GUNUNG PUTRI', '2024-01-12 16:06:42', '2024-01-12 17:05:33', 1),
+(5, 'REPPKU', 'REP OFFICE PEKANBARU', '2024-01-12 16:07:49', '2024-01-12 17:07:35', 1),
+(6, 'REPSBY', 'REP OFFICE SURABAYA', '2024-01-12 16:07:49', '2024-01-12 17:07:35', 1),
+(7, 'BOBPN', 'BRANCH OFFICE BALIKPAPAN', '2024-01-12 16:08:35', '2024-01-12 17:08:17', 1);
 
 -- --------------------------------------------------------
 
@@ -79,8 +94,19 @@ CREATE TABLE `dept` (
   `initial` varchar(10) NOT NULL,
   `name` varchar(50) NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `created_at` datetime NOT NULL
+  `created_at` datetime NOT NULL,
+  `created_by` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `dept`
+--
+
+INSERT INTO `dept` (`id`, `initial`, `name`, `updated_at`, `created_at`, `created_by`) VALUES
+(1, 'HRGA', 'HR & GA', '2024-01-12 16:24:37', '2024-01-12 17:23:57', 1),
+(2, 'QC', 'QUALITY CONTROL', '2024-01-12 16:24:37', '2024-01-12 17:23:57', 1),
+(3, 'PC', 'PROJECT CONTROL', '2024-01-12 16:24:56', '2024-01-12 17:24:40', 1),
+(4, 'PECRANES', 'PE CRANES', '2024-01-12 16:24:56', '2024-01-12 17:24:40', 1);
 
 -- --------------------------------------------------------
 
@@ -91,7 +117,8 @@ CREATE TABLE `dept` (
 CREATE TABLE `goods` (
   `id` int(11) NOT NULL,
   `number` varchar(30) NOT NULL,
-  `desc` varchar(100) NOT NULL,
+  `sn` varchar(100) NOT NULL,
+  `description` varchar(100) NOT NULL,
   `specification` text NOT NULL,
   `id_inv_type` int(11) NOT NULL,
   `id_inv_group` int(11) NOT NULL,
@@ -104,8 +131,18 @@ CREATE TABLE `goods` (
   `id_inv_condition` int(11) NOT NULL,
   `notes` text NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `created_at` datetime NOT NULL
+  `created_at` datetime NOT NULL,
+  `as_dump` tinyint(1) NOT NULL DEFAULT 0,
+  `created_by` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `goods`
+--
+
+INSERT INTO `goods` (`id`, `number`, `sn`, `description`, `specification`, `id_inv_type`, `id_inv_group`, `id_inv_allotment`, `id_inv_branch`, `id_inv_source`, `id_inv_dept`, `year`, `useful_period`, `id_inv_condition`, `notes`, `updated_at`, `created_at`, `as_dump`, `created_by`) VALUES
+(2, 'IT/REG/2024/01/01', 'ABP-1232-AWD3-23FS-234F', 'SOUND SYSTEM MIFA K12', '<p>PROSESOR : <br>MEMORI :<br>HARD DRIVE :</p>', 1, 5, 1, 1, 1, 1, '2024', 5, 1, 'REFF PWR 2023/JKT-0142; REFF PO 2023/JKT-L-0123; RR IT IT/RR/2024/01/4; TES', '2024-01-13 05:14:11', '2024-01-13 12:14:11', 0, 1),
+(3, 'IT/REG/2024/01/02', 'ASDASDSF', 'MOUSE WIRELESS LOGITECH M190', '<p>PROSESOR : <br>MEMORI :<br>HARD DRIVE :</p>', 1, 1, 2, 2, 1, 3, '2024', 2, 1, 'REFF PWR ; REFF PO ;', '2024-01-15 15:03:04', '2024-01-15 22:03:04', 0, 1);
 
 -- --------------------------------------------------------
 
@@ -115,11 +152,28 @@ CREATE TABLE `goods` (
 
 CREATE TABLE `good_incoming` (
   `id` int(11) NOT NULL,
-  `number` varchar(10) NOT NULL,
-  `notes` text NOT NULL,
+  `number` varchar(20) NOT NULL,
+  `notes` text DEFAULT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `created_at` datetime NOT NULL
+  `created_at` datetime NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `as_dump` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `good_incoming`
+--
+
+INSERT INTO `good_incoming` (`id`, `number`, `notes`, `updated_at`, `created_at`, `created_by`, `as_dump`) VALUES
+(2, 'IT/RR/2024/01/1', NULL, '2024-01-10 13:39:13', '2024-01-08 20:09:04', 1, 1),
+(3, 'IT/RR/2024/01/2', NULL, '2024-01-10 13:39:17', '2024-01-08 20:09:34', 1, 1),
+(4, 'IT/RR/2024/01/3', NULL, '2024-01-10 13:39:22', '2024-01-08 20:12:53', 1, 1),
+(5, 'IT/RR/2024/01/4', 'Kebutuhan consumable IT pada bulan januari 2024', '2024-01-10 13:42:50', '2024-01-09 20:37:36', 1, 0),
+(6, 'IT/RR/2024/01/5', 'Kebutuhan PLN indramayu', '2024-01-10 13:38:36', '2024-01-10 19:09:12', 1, 1),
+(7, 'IT/RR/2024/01/6', NULL, '2024-01-10 13:45:00', '2024-01-10 20:45:00', 1, 0),
+(8, 'IT/RR/2024/01/7', NULL, '2024-01-10 14:06:13', '2024-01-10 21:06:13', 1, 0),
+(9, 'IT/RR/2024/01/8', NULL, '2024-01-12 14:06:52', '2024-01-12 21:06:52', 1, 0),
+(10, 'IT/RR/2024/01/9', NULL, '2024-01-12 16:12:28', '2024-01-12 23:12:28', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -130,15 +184,29 @@ CREATE TABLE `good_incoming` (
 CREATE TABLE `good_incoming_details` (
   `id` int(11) NOT NULL,
   `id_incoming` int(11) NOT NULL,
-  `desc` varchar(100) NOT NULL,
-  `sn` varchar(40) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `sn` varchar(100) NOT NULL,
   `pwr` varchar(20) NOT NULL,
   `po` varchar(20) NOT NULL,
   `type` int(11) NOT NULL,
-  `notes` text NOT NULL,
+  `notes` text DEFAULT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `created_at` datetime NOT NULL
+  `created_at` datetime NOT NULL,
+  `as_inv` tinyint(1) NOT NULL DEFAULT 0,
+  `as_dump` tinyint(1) NOT NULL DEFAULT 0,
+  `created_by` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `good_incoming_details`
+--
+
+INSERT INTO `good_incoming_details` (`id`, `id_incoming`, `description`, `sn`, `pwr`, `po`, `type`, `notes`, `updated_at`, `created_at`, `as_inv`, `as_dump`, `created_by`) VALUES
+(1, 5, 'SOUND SYSTEM MIFA K12', 'ABP-1232-AWD3-23FS-234F', '2023/JKT-0142', '2023/JKT-L-0123', 1, 'TES', '2024-01-13 05:14:11', '2024-01-09 21:14:42', 1, 0, 1),
+(2, 5, 'LISENSI AEC 2024', 'ABP-1232-AWD3-23FS-234F1', '2024/JKT-0023', 'MEMO/JKT-0011', 2, 'AEC 2024 UNTUK ENG', '2024-01-13 04:47:20', '2024-01-09 21:28:04', 0, 0, 1),
+(6, 7, 'SOUND SYSTEM MIFA K12', '12312BHJB1JH23B', '2024/JKT-0023', '2023/JKT-L-0123', 2, 'TES', '2024-01-10 13:45:10', '2024-01-10 20:45:10', 0, 0, 1),
+(7, 7, 'LISENSI AEC 2024', '132234-ASDASD-23423DA', '2024/JKT-0023', '2023/JKT-L-0123', 1, 'TES', '2024-01-10 14:02:57', '2024-01-10 21:02:57', 0, 0, 1),
+(8, 7, 'LISENSI SOLIDWORKS 2024', 'ASDADA-2342Q34A-SDDASD', '2024/JKT-0023', '2023/JKT-L-0123', 2, 'TES', '2024-01-10 14:03:34', '2024-01-10 21:03:34', 0, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -149,6 +217,7 @@ CREATE TABLE `good_incoming_details` (
 CREATE TABLE `inv_allotment` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
+  `user_id` int(2) DEFAULT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `created_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -163,8 +232,19 @@ CREATE TABLE `inv_condition` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `created_at` datetime NOT NULL
+  `created_at` datetime NOT NULL,
+  `created_by` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `inv_condition`
+--
+
+INSERT INTO `inv_condition` (`id`, `name`, `updated_at`, `created_at`, `created_by`) VALUES
+(1, 'BAIK', '2024-01-13 05:36:02', '2024-01-13 06:35:49', 1),
+(2, 'KURANG  BAIK', '2024-01-13 05:36:02', '2024-01-13 06:35:49', 1),
+(3, 'RUSAK', '2024-01-13 05:36:13', '2024-01-13 06:36:04', 1),
+(4, 'SCRAPT', '2024-01-13 05:36:13', '2024-01-13 06:36:04', 1);
 
 -- --------------------------------------------------------
 
@@ -176,8 +256,20 @@ CREATE TABLE `inv_group` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `created_at` datetime NOT NULL
+  `created_at` datetime NOT NULL,
+  `created_by` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `inv_group`
+--
+
+INSERT INTO `inv_group` (`id`, `name`, `updated_at`, `created_at`, `created_by`) VALUES
+(1, 'LAPTOP', '2024-01-12 16:11:15', '2024-01-12 17:09:22', 1),
+(2, 'PC', '2024-01-12 16:11:15', '2024-01-12 17:09:22', 1),
+(3, 'PRINTER', '2024-01-12 16:11:45', '2024-01-12 17:11:18', 1),
+(4, 'MIKROTIK', '2024-01-12 16:11:45', '2024-01-12 17:11:18', 1),
+(5, 'SOUND SYSTEM', '2024-01-12 16:11:57', '2024-01-12 17:11:48', 1);
 
 -- --------------------------------------------------------
 
@@ -189,8 +281,18 @@ CREATE TABLE `inv_type` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `created_at` datetime NOT NULL
+  `created_at` datetime NOT NULL,
+  `created_by` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `inv_type`
+--
+
+INSERT INTO `inv_type` (`id`, `name`, `updated_at`, `created_at`, `created_by`) VALUES
+(1, 'GOODS', '2024-01-12 16:13:24', '2024-01-12 17:13:07', 1),
+(2, 'LISENCES', '2024-01-12 16:13:24', '2024-01-12 17:13:07', 1),
+(3, 'CONSUMMABLES', '2024-01-12 16:13:48', '2024-01-12 17:13:27', 1);
 
 -- --------------------------------------------------------
 
@@ -211,6 +313,7 @@ CREATE TABLE `licences` (
   `id_lic_branch` int(2) NOT NULL,
   `id_lic_source` int(2) NOT NULL,
   `notes` text NOT NULL,
+  `user_id` int(2) DEFAULT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `created_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -224,9 +327,31 @@ CREATE TABLE `licences` (
 CREATE TABLE `lic_type` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
+  `user_id` int(2) DEFAULT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `created_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `session_log`
+--
+
+CREATE TABLE `session_log` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `rand_code` varchar(8) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `session_log`
+--
+
+INSERT INTO `session_log` (`id`, `user_id`, `rand_code`, `created_at`) VALUES
+(3, 1, 'ZGgtnYWE', '2023-11-29 09:50:34'),
+(4, 1, 'Fim3MuBY', '2023-12-06 04:52:08');
 
 -- --------------------------------------------------------
 
@@ -238,8 +363,18 @@ CREATE TABLE `source` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `created_at` datetime NOT NULL
+  `created_at` datetime NOT NULL,
+  `created_by` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `source`
+--
+
+INSERT INTO `source` (`id`, `name`, `updated_at`, `created_at`, `created_by`) VALUES
+(1, 'PWR', '2024-01-12 16:28:07', '2024-01-12 17:27:53', 1),
+(2, 'HIBAH', '2024-01-12 16:28:07', '2024-01-12 17:27:53', 1),
+(3, 'LAIN-LAIN', '2024-01-12 16:28:16', '2024-01-12 17:28:09', 1);
 
 -- --------------------------------------------------------
 
@@ -249,6 +384,8 @@ CREATE TABLE `source` (
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(60) DEFAULT NULL,
   `initial` varchar(10) NOT NULL,
   `name` varchar(50) NOT NULL,
   `nik` varchar(15) NOT NULL,
@@ -256,9 +393,17 @@ CREATE TABLE `users` (
   `id_dept` int(11) NOT NULL,
   `id_branch` int(11) NOT NULL,
   `notes` text NOT NULL,
+  `as_admin` int(1) NOT NULL DEFAULT 0,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `created_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `initial`, `name`, `nik`, `position`, `id_dept`, `id_branch`, `notes`, `as_admin`, `updated_at`, `created_at`) VALUES
+(1, 'mahmudi.nurhasan@jpc.co.id', '$2a$12$54lFY47DRTOrUzlOB0lQ7.rP5sgNhenSPI.abJii.VhQisX3sA6Im', 'MHN', 'M NURHASAN MAHMUDI', 'JPC-JKT-825', 'IT SUPPORT', 1, 1, '', 1, '2023-11-29 09:16:18', '2023-11-29 09:28:31');
 
 --
 -- Indexes for dumped tables
@@ -343,6 +488,12 @@ ALTER TABLE `lic_type`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `session_log`
+--
+ALTER TABLE `session_log`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `source`
 --
 ALTER TABLE `source`
@@ -374,31 +525,31 @@ ALTER TABLE `bast_report_details`
 -- AUTO_INCREMENT for table `branch`
 --
 ALTER TABLE `branch`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `dept`
 --
 ALTER TABLE `dept`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `goods`
 --
 ALTER TABLE `goods`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `good_incoming`
 --
 ALTER TABLE `good_incoming`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `good_incoming_details`
 --
 ALTER TABLE `good_incoming_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `inv_allotment`
@@ -410,19 +561,19 @@ ALTER TABLE `inv_allotment`
 -- AUTO_INCREMENT for table `inv_condition`
 --
 ALTER TABLE `inv_condition`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `inv_group`
 --
 ALTER TABLE `inv_group`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `inv_type`
 --
 ALTER TABLE `inv_type`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `licences`
@@ -437,16 +588,22 @@ ALTER TABLE `lic_type`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `session_log`
+--
+ALTER TABLE `session_log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `source`
 --
 ALTER TABLE `source`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
