115416843ef4009f59e08a58d77bfdb574e30593
M N Mahmud (JPC Laptop) <bussines.mnmahmud@gmail.com>
2024-07-03 21:42:16 +0700

    membuat UI untuk data-support.php

--- data-support.php
+++ data-support.php
@@ -29,6 +29,14 @@
     <!-- Jquery CDN -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 
+    <!-- Datatables Jquery -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css" />
+
+    <!-- Place the first <script> tag in your HTML's <head> -->
+    <script src="https://cdn.tiny.cloud/1/qmbej4hb799ztlyxlce0xqdyf3xmgb1ddaike9wg3cf1vx6b/tinymce/6/tinymce.min.js"
+        referrerpolicy="origin"></script>
+
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>
     <link rel="stylesheet" href="dist/css/public.css" />
 </head>
 
@@ -159,37 +167,267 @@
                         <li class="breadcrumb-item active">Data Supports</li>
                     </ol>
 
+                    <div class="row mb-3">
+                        <div class="callout">
+                            Master data for <b>General</b>
+                        </div>
+                        <div class="col-4">
+                            <div class="card">
+                                <div class="card-body table-responsive">
+                                    <table class="display" id="tableTipeBarang">
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Name</th>
+                                                <th>Description</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>Goods</td>
+                                                <td>-</td>
+                                            </tr>
+                                            <tr>
+                                                <td>2</td>
+                                                <td>Lisences</td>
+                                                <td>-</td>
+                                            </tr>
+                                            <tr>
+                                                <td>3</td>
+                                                <td>Consumable</td>
+                                                <td>-</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="card">
+                                <div class="card-body table-responsive">
+                                    <table class="display" id="tableTipeInv">
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Name</th>
+                                                <th>Description</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>Hak Milik Perusahaan</td>
+                                                <td>-</td>
+                                            </tr>
+                                            <tr>
+                                                <td>2</td>
+                                                <td>Sewaan</td>
+                                                <td>-</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="card">
+                                <div class="card-body table-responsive">
+                                    <table class="display" id="tablePeruntukanInv">
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Name</th>
+                                                <th>Description</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>Infrastruktur</td>
+                                                <td>-</td>
+                                            </tr>
+                                            <tr>
+                                                <td>2</td>
+                                                <td>Inventaris User</td>
+                                                <td>-</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-4">
+                            <div class="card">
+                                <div class="card-body table-responsive">
+                                    <table class="display" id="tableAsalUsul">
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Name</th>
+                                                <th>Description</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>PWR</td>
+                                                <td>-</td>
+                                            </tr>
+                                            <tr>
+                                                <td>2</td>
+                                                <td>Hibah</td>
+                                                <td>-</td>
+                                            </tr>
+                                            <tr>
+                                                <td>3</td>
+                                                <td>Lain-Lain</td>
+                                                <td>-</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="card">
+                                <div class="card-body table-responsive">
+                                    <table class="display" id="tableDept">
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Inisial</th>
+                                                <th>Name</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>HRGA</td>
+                                                <td>HR & GA</td>
+                                            </tr>
+                                            <tr>
+                                                <td>2</td>
+                                                <td>QC</td>
+                                                <td>QUALITY CONTROL</td>
+                                            </tr>
+                                            <tr>
+                                                <td>3</td>
+                                                <td>PC</td>
+                                                <td>PROJECT CONTROL</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="card">
+                                <div class="card-body table-responsive">
+                                    <table class="display" id="tableCondition">
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Name</th>
+                                                <th>Description</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>BAIK</td>
+                                                <td>-</td>
+                                            </tr>
+                                            <tr>
+                                                <td>2</td>
+                                                <td>KURANG BAIK</td>
+                                                <td>-</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="callout">
+                        Master data for <b>Daftar Barang</b>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col">
+                            <div class="card">
+                                <div class="card-body table-responsive">
+                                    <table class="display" id="tableGroupInv">
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Kode Barang</th>
+                                                <th>Jenis Barang</th>
+                                                <th>Nama Barang</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>LT01</td>
+                                                <td>LAPTOP</td>
+                                                <td>LAPTOP LENOVO YOGA SLIM 7 PRO - 82MS002YID</td>
+                                            </tr>
+                                            <tr>
+                                                <td>2</td>
+                                                <td>LT02</td>
+                                                <td>LAPTOP</td>
+                                                <td>LAPTOP ASUS ROG STRIX G713QC</td>
+                                            </tr>
+                                            <tr>
+                                                <td>3</td>
+                                                <td>MO01</td>
+                                                <td>MOUSE</td>
+                                                <td>MOUSE LOGITECH M190</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="callout">
+                        Master data for <b>Daftar Lisences</b>
+                    </div>
                     <div class="row">
                         <div class="col">
                             <div class="card">
-                                <div class="card-body">Barang 12</div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="card">
-                                <div class="card-body">Cabang 12</div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="card">
-                                <div class="card-body">Pengguna 12</div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="card">
-                                <div class="card-body">Berita Acara 12</div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="card">
-                                <div class="card-body">Rusak/Scrap 12</div>
+                                <div class="card-body table-responsive">
+                                    <table class="display" id="tableTipeLic">
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Name</th>
+                                                <th>Description</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>Perpetual Lisence</td>
+                                                <td>-</td>
+                                            </tr>
+                                            <tr>
+                                                <td>2</td>
+                                                <td>Subscribtion Lisence</td>
+                                                <td>-</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </main>
 
-            <footer class="py-4 bg-light mt-auto">
+            <footer class="py-4 bg-light mt-5">
                 <div class="container-fluid px-4">
                     <div class="d-flex align-items-center justify-content-center small">
                         <div class="text-muted">Copyright &copy; Your Website 2022</div>
@@ -202,14 +440,31 @@
     </script>
     <script src="dist/temp/js/scripts.js"></script>
     <script src="dist/js/main.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-    <script src="dist/temp/assets/demo/chart-area-demo.js"></script>
-    <script src="dist/temp/assets/demo/chart-bar-demo.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
     <script src="dist/temp/js/datatables-simple-demo.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.2/css/buttons.dataTables.min.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/select/1.5.0/css/select.bootstrap5.min.css" />
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.print.min.js"></script>
+    <script src="https://cdn.datatables.net/select/1.5.0/js/dataTables.select.min.js"></script>
     <script>
     $(document).ready(function() {
         $(".preloader").fadeOut("slow");
+    });
+
+    $(document).ready(function() {
+        $("#tableTipeBarang").DataTable();
+        $("#tableTipeInv").DataTable();
+        $("#tableGroupInv").DataTable();
+        $("#tablePeruntukanInv").DataTable();
+        $("#tableDept").DataTable();
+        $("#tableCondition").DataTable();
+        $("#tableTipeLic").DataTable();
+        $("#tableAsalUsul").DataTable();
     });
     </script>
 </body>
